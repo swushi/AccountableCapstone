2264d44c877f9eb855419b095f37f07a0acb842e
Autrey, Samuel <samautrey7@gmail.com>
2020-02-20 14:15:43 -0600

    Updated Types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,59 @@
+import { Notifications } from "expo";
+
+export type UserID = string;
+export type ReminderID = string;
+export type HabitID = string;
+export type ExpoPushToken = string;
+export type ExpoNotification = Object;
+
 export type User = {
-  uid: string;
+  uid?: UserID;
   firstName: string;
   lastName: string;
   email: string;
+  pushToken?: ExpoPushToken;
+  accountables?: Array<User>;
+};
+
+/**
+ * Lifecylce of habits:
+ * 1. User generates habit(without HabitID)
+ * 2. Push to database and receive HabitID
+ * 3. Pull from database to use during lifespan
+ * 4a. Set habit to inactive when dateEnd is reached
+ * 4b. User deletes habit.
+ */
+export type Habit = {
+  id?: HabitID; // optional because when created on client, will not have id yet
+  type: "make" | "break";
+  title: string;
+  desc: string;
+  active: Boolean; // if the habit is currently on-going
+  dateStart: string;
+  dateEnd: string;
+  stats: Stats;
+  reminders: Array<ReminderID>;
+  accountable: User;
+};
+
+/**
+ * Going to store individual reminders for a user. These will go in their own document in
+ * firebase, and on each day, Firebase Functions will go through and send notifictations
+ * based on what Reminders show up for that day.
+ */
+export type Reminder = {
+  rid: ReminderID;
+  uid: UserID;
+  day: string;
+  time: string;
+  pushToken: ExpoPushToken;
+  notification: ExpoNotification;
+};
+
+/**
+ * Stats for a single habit.
+ */
+export type Stats = {
+  streak: number;
+  timesBroken: number;
 };
