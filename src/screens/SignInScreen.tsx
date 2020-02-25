4323413d2aba493a5644c33d8b5e33065cd24d1d
Autrey, Samuel <samautrey7@gmail.com>
2020-02-25 11:03:58 -0600

    Update SignInScreen.tsx

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -5,7 +5,11 @@ type SignInScreenProps = {
   navigation: any;
 };
 
-type SignInScreenState = {};
+type SignInScreenState = {
+  email: string;
+  password: string;
+  err: any;
+};
 
 class SignInScreen extends React.Component<
   SignInScreenProps,
@@ -13,11 +17,58 @@ class SignInScreen extends React.Component<
 > {
   constructor(props: SignInScreenProps) {
     super(props);
-    this.state = {};
+    this.state = { email: "", password: "", err: null };
   }
 
-  handlePress = () => {
-    this.props.navigation.navigate("SignUp");
+  handlePress = async () => {
+    try {
+      // handle sign up
+      // await this.signInAsync();
+
+      // navigate if okay
+      this.props.navigation.navigate("SignUp");
+    } catch (err) {}
+  };
+
+  signInAsync = async () => {
+    // deref
+    const { navigate } = this.props.navigation;
+    const { email, password, err } = this.state;
+
+    try {
+      // Sign up user
+      const { user } = await firebase.signUp(email, password);
+      const { uid } = user;
+
+      // Sign in user
+      const signIn = firebase.signIn(email, password);
+
+      // create user obj
+      const User: User = {
+        uid,
+        email,
+        firstName: "John",
+        lastName: "Doe"
+      };
+
+      // Initialize user in database
+      const createUser = firebase.createUser(User);
+
+      // Store user globally in Redux
+      // storeUser(User);
+
+      // wait for async signIn and createUser to finish
+      await Promise.all([signIn, createUser]);
+
+      // navigate to MainStack upon success
+      navigate("Main");
+    } catch (err) {
+      // log error
+      alert(err);
+
+      // set error in state
+      this.setState({ err });
+    }
   };
 
   render() {
