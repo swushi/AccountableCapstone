0c57b3af311aca753e1e6eeff4fe3156f080e991
Autrey, Samuel <samautrey7@gmail.com>
2020-02-26 22:26:47 -0600

    Update config

---
+++ src/config/index.d.ts
@@ -0,0 +1,26 @@
+import { Dimensions } from "react-native";
+const { height, width } = Dimensions.get("window");
+
+export const Colors = {
+  primary: "#8b67f0",
+  background: "#f1f1f1",
+  textPrimary: "#424242"
+};
+
+export const Design = {
+  padding: 12,
+  height,
+  width,
+  roundness: 12
+};
+
+export const FirebaseConfig = {
+  apiKey: "AIzaSyBk1lFHbxoNqWAqwexSHne_fPNz7qCsiKQ",
+  authDomain: "accountable-ba5c6.firebaseapp.com",
+  databaseURL: "https://accountable-ba5c6.firebaseio.com",
+  projectId: "accountable-ba5c6",
+  storageBucket: "accountable-ba5c6.appspot.com",
+  messagingSenderId: "806395418095",
+  appId: "1:806395418095:web:6124cb947e2df6b779121a",
+  measurementId: "G-33XWPTKTK5"
+};
--- src/config/index.js
+++
@@ -1,2 +0,0 @@
-export { default as FirebaseConfig } from "./FirebaseConfig";
-export { default as Config } from "./Config";
