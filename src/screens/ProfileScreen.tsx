3a670290e2bf08b46f1b5fd919912cc99956243a
miyere <44795076+miyere@users.noreply.github.com>
2020-03-22 17:24:44 -0500

    Initial commit to profile page

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { Design, Colors, validateEmail } from "../config";
+import * as Animatable from "react-native-animatable";
 
 export interface ProfileScreenProps {}
 
@@ -9,18 +11,47 @@ class ProfileScreen extends React.Component<
   ProfileScreenProps,
   ProfileScreenState
 > {
+  containerRef: any;
   constructor(props: ProfileScreenProps) {
     super(props);
     this.state = {};
   }
 
+
   render() {
     return (
-      <View>
-        <Text>ProfileScreen Component</Text>
+      <View style={{ flex: 1 }}>
+        <Animatable.View
+          style={styles.container}
+          ref={ref => (this.containerRef = ref)}
+          useNativeDriver
+        >
+          <View style={styles.topBar}>
+            <Text style={styles.headerText}> Accountable</Text>
+          </View>
+        </Animatable.View>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: Design.padding,
+    paddingTop: Design.padding,
+    justifyContent: "space-around",
+    backgroundColor: Colors.background
+    //justifyContent: "center",
+  },
+  topBar: {
+    //headerStyle:
+  },
+  headerText: {
+      fontSize: 20,
+      textAlign: "center",
+      margin: 10,
+      fontWeight: "bold"
+  }
+});
 export default ProfileScreen;
