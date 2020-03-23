5b6e129385cb9464834504c4998141034fb25dc8
Sam <samautrey7@gmail.com>
2020-03-23 10:40:42 -0500

    Cache fonts

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
 import AppContainer from "./src/navigation";
 import * as firebase from "./src/firebase";
 
 firebase.init();
 
 class App extends React.Component {
-  componentDidMount() {}
+  state = {
+    isReady: false
+  };
+
+  /**
+   * Cache all fonts and images before rendering application
+   */
+  async _cacheResourcesAsync() {
+    const fonts = Font.loadAsync({
+      "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+      "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf")
+    });
+
+    return Promise.all([fonts]);
+  }
 
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
     return <AppContainer />;
   }
 }
