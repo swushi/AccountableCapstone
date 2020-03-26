72eba9e406330e32a67f17ad4f346c06d3450362
Sam <samautrey7@gmail.com>
2020-03-26 11:28:23 -0500

    Fixed corruption issue

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Header } from "./Header";
 export { default as HabitButton } from "./HabitButton";
 export { default as DateTimePickerModal } from "./DateTimePickerModal";
-export { default as Toggle } from "./Toggle";
+export { default as ToggleButton } from "./ToggleButton";
 export { default as PresetHabitListItem } from "./PresetHabitListItem";
